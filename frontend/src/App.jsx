8d6ef3ea76656fb9312482c2f4c66c1d36fc047f
vishnu563 <vishnuprasanth050603@gmail.com>
2024-12-04 16:11:17 +0530

    Backend Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
+import DeleteBooks from './pages/DeleteBooks'
 
 const App = () => {
   return (
-    <div className='bg-red-400'>App</div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/books/create" element={<CreateBooks/>} />
+        <Route path="/books/details/:id" element={<ShowBook/>} />
+        <Route path="/books/edit/:id" element={<EditBook/>} />
+        <Route path="/books/delete/:id" element={<DeleteBooks/>} />
+      </Routes>
   )
 }
 
