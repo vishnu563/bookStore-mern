11819e5b6cd64765b4207ce27144508f0e736fef
vishnu563 <vishnuprasanth050603@gmail.com>
2024-12-04 15:59:51 +0530

    backend created

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from "express";
-import {PORT,mongoDBURL} from "./config.js"
 import mongoose from "mongoose";
+import cors from "cors";
+import {PORT,mongoDBURL} from "./config.js";
+import bookRouter from "./routes/booksRoute.js";
+
 
 const app = express();
+app.use(express.json());
+app.use(cors())
 
 app.get('/',(req,res)=>{
     return res.status(200).send("HELLO")
 })
+
+app.use('/books',bookRouter);
 
 
 mongoose.connect(mongoDBURL)
